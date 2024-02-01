149f1fce581cbc54cfdfcd8a9a724d7a8ae688d1
xxrizu <97385084+xxrizu@users.noreply.github.com>
2024-02-01 17:56:50 +0900

    update 10

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
-console.log("script")
+const inputName = document.getElementById('input-name');
+const inputEmail = document.getElementById('input-email');
+
+const button = document.getElementById('tombol-submit');
+
+button.addEventListener("click", function () {
+  let nama = inputName.value;
+  let email = inputEmail.value;
+  
+  console.log({nama,email})
+})
+
+if (!nama) {
+  const nameError = document.getElementById('name-error');
+  nameError.classList.add('error-message');
+
+  nameError.innerText = "nama harus diisi";
+}
+
+if (!email) {
+  const emailError = document.getElementById('email-error');
+  emailError.classList.add('error-message');
+
+  emailError.innerText = "email harus diisi";
+}
+
+
+
+
+
